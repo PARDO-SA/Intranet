c47f66c7007b49690a8982c6803f7b69050a9bd7
Victor Malvasio <victormalvasio@gmail.com>
2018-04-27 10:25:49 -0300

    Traduccion

--- Intranet/Scripts/table.js
+++ Intranet/Scripts/table.js
@@ -1,3 +1,27 @@
 $(document).ready(function () {
-    $('#tabla').dataTable();
+    $('#tabla').dataTable({
+        "language": {
+            //"url": "//cdn.datatables.net/plug-ins/1.10.16/i18n/Spanish.json",
+            "sProcessing": "Procesando...",
+            "sLengthMenu": "Mostrar _MENU_ registros",
+            "sZeroRecords": "No se encontraron resultados",
+            "sEmptyTable": "Ningún dato disponible en esta tabla",
+            "sInfo": "Mostrando _START_ al _END_ de _TOTAL_ registros ",
+            "sInfoEmpty": "Mostrando registros del 0 al 0 de un total de 0 registros",
+            "sInfoFiltered": "(filtrado de un total de _MAX_ registros)",
+            "sInfoPostFix": "",
+            "sSearch": "Buscar:",
+            "sUrl": "",
+            "sInfoThousands": ",",
+            "sLoadingRecords": "Cargando...",
+            "oPaginate": {
+                "sFirst": "Primero",
+                "sLast": "Último",
+                "sNext": "Siguiente",
+                "sPrevious": "Anterior"
+            }
+        },
+        "bLengthChange": false,
+        "pageLength": 20
+    });
 });
